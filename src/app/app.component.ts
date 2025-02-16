05f9479b7bbd84d856a311c488bcef18ff9ee79d
Rholme-Stack <r9london@hotmail.com>
2025-02-16 22:56:59 +0100

    favicon, bg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar/navbar.component";
 import { HomePageComponent } from "./home/home-page/home-page.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HomePageComponent],
+  imports: [RouterOutlet, NavbarComponent, HomePageComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
