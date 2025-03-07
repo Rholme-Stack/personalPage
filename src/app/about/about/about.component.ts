842b4e56cbbe766579600cf137c1084ec97ce72b
Rodrigo <rodrigo.holme.lopes@avanade.com>
2025-03-07 23:20:46 +0100

    TEST LIST

--- src/app/about/about/about.component.ts
+++ src/app/about/about/about.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { routes } from '../../app.routes';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  standalone: true,
-  imports: [TranslateModule],
+  standalone: true,  
+  imports: [TranslateModule, RouterModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
