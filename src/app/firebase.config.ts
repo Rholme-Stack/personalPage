64f68c9ec0e3ca2c6b2f0bb401c6d4cdab382af7
RodrigoHolme <r9holme@gmail.com>
2026-04-08 13:20:43 +0200

    cambios

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDcKp",
+  apiKey: "AIzaSyDcKpeeDUs4GJYlonuQUsom3uvsY222oJs",
   authDomain: "personalpage-88072.firebaseapp.com",
   projectId: "personalpage-88072",
   storageBucket: "personalpage-88072.firebasestorage.app",
   messagingSenderId: "978294741614",
-  appId: "1:978294741614:web:dbfe9f681e",
-  measurementId: "G-MY5S"
+  appId: "1:978294741614:web:dbfe9f681e54bc987ee47f",
+  measurementId: "G-MY5SZM5TJ9"
 };
 
